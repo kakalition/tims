d9fe73e2ab68f8d64e7620555423c5a1301c5957
Kaka <kakalition@gmail.com>
2021-11-26 18:39:43 +0700

    fix broken commit

--- lib/routes/main_route.dart
+++ lib/routes/main_route.dart
@@ -5,7 +5,8 @@ import 'package:tims/utils.dart';
 import 'package:tims/widgets/navigation_drawer_tile.dart';
 
 class MainRoute extends StatelessWidget {
-  const MainRoute({Key? key}) : super(key: key);
+  MainRoute({Key? key}) : super(key: key);
+  List<Widget> screens = [];
 
   @override
   Widget build(BuildContext context) {
@@ -26,77 +27,85 @@ class MainRoute extends StatelessWidget {
             IconButton(onPressed: () {}, icon: const Icon(LineIcons.cog))
           ],
         ),
-        drawer: Drawer(
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Spacer(flex: 1),
-                Flexible(
-                  flex: 2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(LineIcons.user, size: 120),
-                      const SizedBox(height: 50),
-                      timsTextBuilderWithStyle(
-                          text: "Kaka",
-                          textStyle: Theme.of(context).textTheme.headline4!),
-                      const SizedBox(height: 10),
-                      timsTextBuilderWithStyle(
-                          text: "TIMS Developer",
-                          textStyle: Theme.of(context).textTheme.headline6!),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 50),
-                Expanded(
-                  flex: 3,
-                  child: ListView(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 8),
-                        child: NavigationDrawerTile(
-                          icon: LineIcons.clock,
-                          tileLabel: 'Timer',
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 8),
-                        child: NavigationDrawerTile(
-                          icon: LineIcons.flag,
-                          tileLabel: 'Stopwatch',
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 8),
-                        child: NavigationDrawerTile(
-                          icon: LineIcons.list,
-                          tileLabel: 'Timer List',
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 8),
-                        child: NavigationDrawerTile(
-                          icon: LineIcons.history,
-                          tileLabel: 'History',
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+        drawer: _MainRouteDrawer(),
+        body: Container());
+  }
+}
+
+class _MainRouteDrawer extends StatelessWidget {
+  const _MainRouteDrawer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Spacer(flex: 1),
+            Flexible(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(LineIcons.user, size: 120),
+                  const SizedBox(height: 50),
+                  timsTextBuilderWithStyle(
+                      text: "Kaka",
+                      textStyle: Theme.of(context).textTheme.headline4!),
+                  const SizedBox(height: 10),
+                  timsTextBuilderWithStyle(
+                      text: "TIMS Developer",
+                      textStyle: Theme.of(context).textTheme.headline6!),
+                ],
+              ),
             ),
-          ),
+            const SizedBox(height: 50),
+            Expanded(
+              flex: 3,
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 8),
+                    child: NavigationDrawerTile(
+                      icon: LineIcons.clock,
+                      tileLabel: 'Timer',
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 8),
+                    child: NavigationDrawerTile(
+                      icon: LineIcons.flag,
+                      tileLabel: 'Stopwatch',
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 8),
+                    child: NavigationDrawerTile(
+                      icon: LineIcons.list,
+                      tileLabel: 'Timer List',
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 8),
+                    child: NavigationDrawerTile(
+                      icon: LineIcons.history,
+                      tileLabel: 'History',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
-        ));
+      ),
+    );
   }
 }
