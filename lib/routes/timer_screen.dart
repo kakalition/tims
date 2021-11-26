232e423334317542c00c8002170a03519ca431bc
Kaka <kakalition@gmail.com>
2021-11-26 19:05:55 +0700

    Create placeholder for more detailed widget

--- lib/routes/timer_screen.dart
+++ lib/routes/timer_screen.dart
@@ -6,18 +6,70 @@ class TimerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: MediaQuery.of(context).size.width * 0.4,
-          width: MediaQuery.of(context).size.width * 0.4,
-          decoration: BoxDecoration(
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          // Time Circle
+          Container(
+            height: MediaQuery.of(context).size.width * 0.6,
+            width: MediaQuery.of(context).size.width * 0.6,
+            decoration: BoxDecoration(
+              border: Border.all(width: 12, color: whiteColorDarkTheme),
+              borderRadius: BorderRadius.circular(
+                MediaQuery.of(context).size.width * 0.6,
+              ),
+            ),
+          ),
+
+          // Play/Stop Button
+          Container(
+            height: MediaQuery.of(context).size.width * 0.3,
+            width: MediaQuery.of(context).size.width * 0.3,
+            decoration: BoxDecoration(
               border: Border.all(width: 6, color: whiteColorDarkTheme),
               borderRadius: BorderRadius.circular(
-                MediaQuery.of(context).size.width * 0.4,
-              )),
-        )
-      ],
+                MediaQuery.of(context).size.width * 0.3,
+              ),
+            ),
+          ),
+
+          // Restart Button
+          Container(
+            height: MediaQuery.of(context).size.width * 0.2,
+            width: MediaQuery.of(context).size.width * 0.2,
+            decoration: BoxDecoration(
+              border: Border.all(width: 6, color: whiteColorDarkTheme),
+              borderRadius: BorderRadius.circular(
+                MediaQuery.of(context).size.width * 0.25,
+              ),
+            ),
+          ),
+
+          // FAB
+          Material(
+            color: whiteColorDarkTheme,
+            borderRadius:
+                BorderRadius.circular(MediaQuery.of(context).size.width * 0.25),
+            child: InkWell(
+              onTap: () {},
+              borderRadius: BorderRadius.circular(
+                  MediaQuery.of(context).size.width * 0.2),
+              child: Container(
+                height: MediaQuery.of(context).size.width * 0.2,
+                width: MediaQuery.of(context).size.width * 0.2,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    MediaQuery.of(context).size.width * 0.2,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
